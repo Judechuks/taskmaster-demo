d0f1e16d7d2f8007614e6a404c23b83014c5f7a8
Jude Chuks <judechuks26@gmail.com>
2024-12-01 15:39:15 +0100

    environment variable

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ const authenticateToken = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" }); // No token, unauthorized
   }
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+  jwt.verify(token, `${process.env.JWT_SECRET_KEY}`, (err, user) => {
     if (err) {
       return res.status(403).json({ message: "Invalid token" }); // Token invalid, forbidden
     }
